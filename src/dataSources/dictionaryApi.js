28e0ba1d89e92cac7799fd07b9cf17d761988c29
sheva3666 <ivan.shevchenko@sap.com>
2023-05-05 10:28:22 +0200

    working with auth

--- src/dataSources/dictionaryApi.js
+++ src/dataSources/dictionaryApi.js
@@ -1,12 +1,31 @@
-const { RESTDataSource } = require("apollo-datasource-rest");
+const { RESTDataSource, RequestOptions } = require("apollo-datasource-rest");
 
 class DictionaryAPI extends RESTDataSource {
   constructor() {
     super();
+    this.token = options.token;
     this.baseURL = "http://localhost:8082/";
   }
+
+  // willSendRequest(request) {
+  //   this.context.request.headers = {}
+  //   request.headers.set(
+  //     "authorization",
+  //     this.context.request.headers.authorization
+  //   );
+  //   request.headers.set("Accept", "application/json");
+  //   request.headers.set(
+  //     "user-agent",
+  //     this.context.request.headers["user-agent"]
+  //   );
+  // }
+
+  willSendRequest(_path, request) {
+    request.headers["authorization"] = this.token;
+  }
+
   getWords(user, language, translateLanguage) {
-    return this.get(`words/${user}/${language}/${translateLanguage}`);
+    return this.get(`words/${user}/${language}/${translateLanguage}/u`);
   }
 
   getUser(user, password) {
